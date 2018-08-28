51061e9aaf533cdfe22016b9fd3e728b762d50f0
Corner-fly <42516663+Corner-fly@users.noreply.github.com>
2018-08-28 21:28:12 +0800

    更改了请求数据的接口

--- js/index.js
+++ js/index.js
@@ -2,10 +2,11 @@ $(function(){
 	//页面加载完成之后执行
 	pageInit();
 	$.ajax({
-		url:'data/JSONData.json',
-        dataType: "json",
-        async: false,
-        cache: false,
+		//url:'data/JSONData.json',
+		url:'https://corner-fly.github.io/demo/data/JSONData.json',
+                dataType: "json",
+                async: false,
+                cache: false,
 		success:function(data){
 			console.log(data);
 		}
@@ -15,7 +16,7 @@ function pageInit(){
 	//创建jqGrid组件
 	jQuery("#list2").jqGrid(
 			{
-				height: $(window).height() - 135,// 	表格高度，可以是数字，像素值或者百分比
+				height: $(window).height() - 300,// 	表格高度，可以是数字，像素值或者百分比
 				autowidth: true,//如果为ture时，则当表格在首次被创建时会根据父元素比例重新调整表格宽度。如果父元素宽度改变，为了使表格宽度能够自动调整则需要实现函数：setGridWidth;，默认值false
 				url : 'data/JSONData.json',//组件创建完成之后请求数据的url
 				datatype : "json",//请求数据返回的类型。可选json,xml,txt
